1eec9f037dc3310906fce8eb1c077c04ca48c1d6
Juan Correa <juan.correa@fieam.org.br>
2025-03-11 14:35:38 -0400

    The second commit

---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -0,0 +1,36 @@
+﻿using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRecipeBook.Application.UseCases.User.Register
+{
+    public class RegisterUserUseCase
+    {
+        public ResponseRegisteredUserJson Execute(RequestRegisterUserJson request)
+        {
+            Validate(request);
+
+            return new ResponseRegisteredUserJson
+            {
+                Name = request.Name,
+            };
+        }
+
+        private void Validate(RequestRegisterUserJson request)
+        {
+            var validator = new RegisterUserValidator();
+            var result = validator.Validate(request);
+
+            if (result.IsValid == false)
+            {
+                var erroMessages = result.Errors.Select(e => e.ErrorMessage);
+
+                throw new Exception();
+            }
+        }
+    }
+}
---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRecipeBook.Application.UseCases.User.Register
+{
+    internal class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson>
+    {
+        public RegisterUserValidator() { 
+            RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceMessageException.NAME_EMPTY);
+            RuleFor(user => user.Email).NotEmpty().WithMessage("O e-mail não pode ser vazio");
+            RuleFor(user => user.Email).EmailAddress();
+            RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(6);
+        }
+    }
+}
